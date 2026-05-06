3af2155896143bf0d66f81d5eb5858b0b2a7120c
wutdahack1337 <wutdahack1337@gmail.com>
2026-05-06 20:22:13 +0700

    chore

--- server/routes/invoice.js
+++ server/routes/invoice.js
@@ -6,16 +6,22 @@ const router = Router();
 
 // Generate invoice
 router.post("/", (request, response) => {
-    const { roomId, roomRent, electricityConsumption, electricityPrice, waterConsumption, waterPrice } = request.body;
+    let { roomId, roomRent, electricityConsumption, electricityPrice, waterConsumption, waterPrice } = request.body;
     if (!roomId || electricityConsumption == null || waterConsumption == null || electricityPrice == null || waterPrice == null || roomRent == null) {
         return response.status(400).json({ error: "missing some required fields" });
     }
+
+    roomRent = Number(roomRent);
+    electricityConsumption = Number(electricityConsumption);
+    waterConsumption = Number(waterConsumption);
+    electricityPrice = Number(electricityPrice);
+    waterPrice = Number(waterPrice);
 
     const electricityBill = electricityConsumption * electricityPrice;
     const waterBill = waterConsumption * waterPrice;
     const totalBill = electricityBill + waterBill + roomRent;
 
-    response.status(201).json({ roomId, roomRent, electricityBill, waterBill, totalBill });
+    response.status(200).json({ roomId, roomRent, electricityBill, waterBill, totalBill });
 });
 
 export default router;
